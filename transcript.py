47dc527165745ac1a44d5b63724982e2ae46b2c5
akshay_s__22 <88267526+Chavan22@users.noreply.github.com>
2026-03-01 14:09:27 +0530

    Update transcript.py

--- transcript.py
+++ transcript.py
@@ -1,27 +1,3 @@
-# from youtube_transcript_api import YouTubeTranscriptApi
-# import re
-
-# def extract_video_id(url):
-#     pattern = r"(?:v=|\/)([0-9A-Za-z_-]{11}).*"
-#     match = re.search(pattern, url)
-#     if match:
-#         return match.group(1)
-#     return None
-
-# def get_transcript(url):
-#     video_id = extract_video_id(url)
-#     if not video_id:
-#         raise ValueError("Invalid YouTube URL")
-
-#     transcript = YouTubeTranscriptApi.get_transcript(video_id)
-
-#     full_text = ""
-#     for entry in transcript:
-#         full_text += f"[{int(entry['start'])} sec] {entry['text']} "
-
-#     return full_text
-
-
 import yt_dlp
 import whisper
 import os
