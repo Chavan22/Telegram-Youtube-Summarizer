ef8d73362a78cef1475d2f1b5f4af89753642929
akshay_s__22 <88267526+Chavan22@users.noreply.github.com>
2026-03-01 13:22:20 +0530

    Delete test_transcript.py

--- test_transcript.py
+++
@@ -1,7 +0,0 @@
-from youtube_transcript_api import YouTubeTranscriptApi
-
-video_id = "dQw4w9WgXcQ"  # test video
-
-transcript = YouTubeTranscriptApi.get_transcript(video_id)
-
-print("Transcript length:", len(transcript))
